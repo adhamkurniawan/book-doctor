c1db5f1934fb014823fc9537d22373a00cd620fb
Adam Kurniawan <dmkrnwn@gmail.com>
2016-04-28 10:14:34 +0700

    Add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  DayShift.create(day_shift: "Monday - Friday")
+  DayShift.create(day_shift: "Monday - Friday")
+  DayShift.create(day_shift: "Saturday - Sunday")
+
+  HourShift.create(hour_shift: "08:00 - 16:00")
+  HourShift.create(hour_shift: "13:00 - 22:00")
+  HourShift.create(hour_shift: "09:00 - 15:00")
